8fa20714868c50437211577efbaf2f880d0e1a17
Marcos Henrique Soares Dutra <mhredbluz@gmail.com>
2023-04-12 23:15:10 -0300

    Add backend codes

---
+++ src/main/java/com/codewithmarcos/fullstackbackend/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.codewithmarcos.fullstackbackend.controller;
+
+import com.codewithmarcos.fullstackbackend.model.User;
+import com.codewithmarcos.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
